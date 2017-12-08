d6e2f2a781bc39c5c2220fbeaf438cd528589aaa
KPattonb <32140030+KPattonb@users.noreply.github.com>
2017-12-08 10:01:36 -0800

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,9 @@
+#include "student.h"
+
+Student::Student(int _stud) {
+  stud = _stud;
+}
+
+int Student::getStudent() {
+  return stud;
+}
---
+++ student.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#ifndef STUDENT_H
+#define STUDENT_H
+
+using namespace std;
+
+class Student {
+ public:
+  Student(int);
+  int getStudent();
+
+ protected:
+  int stud;
+  
+};
+
+#endif
